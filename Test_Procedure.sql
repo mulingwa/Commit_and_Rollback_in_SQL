4524a8bf231c4bca5d2b9717b79fd5a74147208c
Mulingwa <84895131+mulingwa@users.noreply.github.com>
2022-10-11 22:02:44 +0300

    Test for IBM class

---
+++ Test_Procedure.sql
@@ -0,0 +1,56 @@
+SELECT * FROM BANKACCOUNTS where Accountname = 'James';
+
+--Create a stored procedure TRANSACTION_JAMES to execute a transaction based on the following scenario: *First buy James 4 pairs of Trainers from ShoeShop. 
+--Update his balance as well as the balance of ShoeShop. 
+--Also, update the stock of Trainers at ShoeShop. 
+--Then attempt to buy James a pair of Brogues from ShoeShop. 
+--If any of the UPDATE statements fail, the whole transaction fails. 
+--You will roll back the transaction. 
+--Commit the transaction only if the whole transaction is successful. 
+
+--#SET TERMINATOR @
+CREATE PROCEDURE TRANSACTION_JAMES                          -- Name of this stored procedure routine
+
+LANGUAGE SQL                                                -- Language used in this routine 
+MODIFIES SQL DATA                                           -- This routine will only write/modify data in the table
+
+BEGIN
+
+        DECLARE SQLCODE INTEGER DEFAULT 0;                  -- Host variable SQLCODE declared and assigned 0
+        DECLARE retcode INTEGER DEFAULT 0;                  -- Local variable retcode with declared and assigned 0
+        DECLARE CONTINUE HANDLER FOR SQLEXCEPTION           -- Handler tell the routine what to do when an error or warning occurs
+        SET retcode = SQLCODE;                              -- Value of SQLCODE assigned to local variable retcode
+        
+        UPDATE BankAccounts
+        SET Balance = Balance-1200
+        WHERE AccountName = 'James';
+        
+        UPDATE BankAccounts
+        SET Balance = Balance+ 1200
+        WHERE AccountName = 'Shoe Shop';
+        
+        UPDATE ShoeShop
+        SET Stock = Stock-4
+        WHERE Product = 'Trainers';
+        
+        UPDATE BankAccounts
+        SET Balance = Balance-150
+        WHERE AccountName = 'Rose';
+
+        
+        IF retcode < 0 THEN                                  --  SQLCODE returns negative value for error, zero for success, positive value for warning
+            ROLLBACK WORK;
+        
+        ELSE
+            COMMIT WORK;
+        
+        END IF;
+        
+END
+@ 
+
+CALL TRANSACTION_JAMES;  -- Caller query
+
+SELECT * FROM BankAccounts;
+
+SELECT * FROM ShoeShop;                                                           -- Routine termination character
